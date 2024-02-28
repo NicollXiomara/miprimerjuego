602c008ee0715f52d53201b3f745cfdf5bd4a182
NicollXiomara <161613201+NicollXiomara@users.noreply.github.com>
2024-02-28 10:26:03 -0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,40 @@
+
+
+let nclicks = 0;
+const imagen = document.querySelector(".imagen");
+const intruso = document.querySelector("#intruso");
+const clicks = document.querySelector(".clicks")
+const alerta = document.querySelector(".alerta")
+const alertaMensaje = document.querySelector(".alerta-mensaje")
+
+intruso.style.backgroundcolor = "righ(0,0,0,0)";
+
+intruso.addEventListener("click", function () {
+   imagen.style.border = "10px solid green"
+   imagen.style.borderRadius = "5%"
+   imagen.style.backgroundcolor = "green";
+   alerta.style.display = "flex"
+    if (nclicks == 1) {
+        alertaMensaje.textContent = "Eres crack encontraste el impostor a la primera";
+        } else if (nclicks <11) {
+        alertaMensaje.textContent = `Buen trabajo, encontraste al impostor despues de ${nclicks}`;
+    }  else{
+        alertaMensaje.textContent = "Eres un ciego, pero igual lo lograste";
+    }
+})
+
+imagen.addEventListener("click", function () {
+    nclicks = nclicks + 1;
+    if (nclicks == 1) {
+       clicks.style.color = "green"
+        } else if (nclicks <11) {
+            clicks,style.color = "yellow"
+    }  else{
+        clicks.style.color = "red"
+    }
+    clicks.textContent = `Clicks: ${nclicks}`;
+})
+
+function recargar(){
+    location.reload()
+}
